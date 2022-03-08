3bf386bcdc2435df1012b35fae36955bab7d2811
dhaef <derekhaefner@gmail.com>
2022-03-07 19:33:32 -0600

    New email

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,26 +1,27 @@
-const nodemailer = require('nodemailer');
-import AWS from 'aws-sdk';
+import nodemailer from 'nodemailer';
+import sendgridTransport from 'nodemailer-sendgrid-transport';
+// import AWS from 'aws-sdk';
 
-AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+// AWS.config.update({
+//   region: 'us-east-1',
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// });
 
 // async..await is not allowed in global scope, must use a wrapper
 export async function sendEmail(email: string, msg: string, subject?: string) {
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    SES: new AWS.SES({
-      apiVersion: '2010-12-01',
-    }),
-  });
+  let transporter = nodemailer.createTransport(
+    sendgridTransport({
+      auth: {
+        api_key: process.env.SEND_GRID_KEY,
+      },
+    })
+  );
   // let transporter = nodemailer.createTransport({
-  //   host: 'smtp.office365.com',
-  //   port: '587',
-  //   auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASSWORD },
-  //   secureConnection: false,
-  //   tls: { ciphers: 'SSLv3' },
+  //   SES: new AWS.SES({
+  //     apiVersion: '2010-12-01',
+  //   }),
   // });
 
   // send mail with defined transport object
